dcf6a4c3ecd8aade19efe2cd75766bcf8a699955
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-05-06 23:39:27 +0200

    USe param destruction in merge sort

--- clojure/sorting/src/sorting/merge.clj
+++ clojure/sorting/src/sorting/merge.clj
@@ -28,6 +28,15 @@
                 (recur (inc i) j (conj m (nth v i)))
                 (recur i (inc j) (conj m (nth w j))))))))
 
+(defn do-merge-destruct [v w m]
+    (let [[v1 & vn] v
+          [w1 & wn] w]
+      (cond
+        (nil? v1) (concat m w)
+        (nil? w1) (concat m v)
+        (< v1 w1) (recur vn w (conj m v1))
+        :else (recur v wn (conj m w1)))))
+
 (defn merge-sort [v]
   (if (<= (count v) 1)
     v
@@ -35,4 +44,5 @@
            split (split-at half v)
            v1 (merge-sort (first split))
            v2 (merge-sort (second split))]
-      (do-merge-cond (into [] v1) (into [] v2)))))
+      ;(do-merge-cond (into [] v1) (into [] v2))
+      (do-merge-destruct (into [] v1) (into [] v2) []))))
