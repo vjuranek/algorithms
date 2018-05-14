5e37eee1b7805c43c24c66db89ad78dc41b573e6
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-05-15 00:02:55 +0200

    Test all implemented merge sort methods

--- clojure/sorting/test/sorting/merge_test.clj
+++ clojure/sorting/test/sorting/merge_test.clj
@@ -5,3 +5,11 @@
 (deftest merge-test
   (testing "Merge sort"
            (is (= (range 10) (merge-sort(shuffle (range 10)))))))
+
+(deftest merge-cond-test
+  (testing "Merge sort using cond"
+           (is (= (range 10) (merge-sort-cond(shuffle (range 10)))))))
+
+(deftest merge-destruct-test
+  (testing "Merge sort argument destruction"
+           (is (= (range 10) (merge-sort-desctruct(shuffle (range 10)))))))
